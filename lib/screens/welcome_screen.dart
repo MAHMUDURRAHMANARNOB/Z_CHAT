714c5b1007264765338ef5b8c6d9aa92189476b2
MAHMUDURRAHMANARNOB <mahmudurrahman2023@gmail.com>
2023-09-24 16:29:57 +0600

    animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -6,15 +6,41 @@ import 'package:z_chat/screens/registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = "welcome_screen";
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(seconds: 1),
+      // upperBound: 50,
+      vsync: this,
+    );
+
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(animationController);
+
+    animationController.forward();
+    animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
